688d56d126a39d257d6a3e02f5e7ec38d03963cd
Joe Block <jpb@unixorn.net>
2014-10-30 07:21:24 -0700

    Add gpg alias, zurl function

--- jpb.plugin.zsh
+++ jpb.plugin.zsh
@@ -195,6 +195,9 @@ alias gpush="git push"
 alias grebase="git rebase -i"
 alias hlog='git log --all --date-order --graph --date=short --format="%C(green)%H%Creset %C(yellow)%an%Creset %C(blue bold)%ad%Creset %C(red bold)%d%Creset%s"'
 
+# GPG stuff
+alias sign='gpg --detach-sign --armor'
+
 # my common tyops
 alias ..="cd .."
 alias gerp='grep'
@@ -592,3 +595,49 @@ function osx_major_version(){
 }
 
 alias asroot='sudo $(fc -ln -1)'
+
+# show newest files
+# http://www.commandlinefu.com/commands/view/9015/find-the-most-recently-changed-files-recursively
+newest (){
+  find . -type f -printf '%TY-%Tm-%Td %TT %p\n' | \
+  grep -v cache | \
+  grep -v ".hg" | grep -v ".git" | \
+  sort -r | \
+  less
+}
+
+# print a separator banner, as wide as the terminal
+function hr {
+  print ${(l:COLUMNS::=:)}
+}
+
+# urlencode text
+function urlencode {
+  print "${${(j: :)@}//(#b)(?)/%$[[##16]##${match[1]}]}"
+}
+
+# Create short urls via http://goo.gl using curl(1).
+# Contributed back to grml zshrc
+# API reference: https://code.google.com/apis/urlshortener/
+function zurl {
+  if [[ -z $1 ]]; then
+    print "USAGE: $0 <URL>"
+    return 1
+  fi
+
+  local url=$1
+  local api="https://www.googleapis.com/urlshortener/v1/url"
+  local data
+
+  # Prepend "http://" to given URL where necessary for later output.
+  if [[ $url != http(s|)://* ]]; then
+    url="http://$url"
+  fi
+  local json="{\"longUrl\": \"$url\"}"
+
+  data=$(curl --silent -H "Content-Type: application/json" -d $json $api)
+  # Match against a regex and print it
+  if [[ $data =~ '"id": "(http://goo.gl/[[:alnum:]]+)"' ]]; then
+    print $match
+  fi
+}
