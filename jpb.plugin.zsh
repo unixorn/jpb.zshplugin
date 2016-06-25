6d2545792237e5a2bd93a6b074fb8de861677109
Joe Block <jpb@unixorn.net>
2016-06-25 09:18:17 -0700

    Add pgs and pgrep

--- jpb.plugin.zsh
+++ jpb.plugin.zsh
@@ -604,3 +604,19 @@ epochdays() {
   fi
   printf "%s\n" "$(( epoch / 86400 ))"
 }
+
+# Perly stuff from https://github.com/massa/massazsh/blob/master/massa.sh
+
+# pgs - Perl Global Substitution
+# find pattern    = 1st arg
+# replace pattern = 2nd arg
+# filename      = 3rd arg
+pgs() { # [find] [replace] [filename]
+    perl -i.orig -pe 's/'"$1"'/'"$2"'/g' "$3"
+}
+
+
+# Perl grep, because 'grep -P' is terrible. Lets you work with pipes or files.
+prep() { # [pattern] [filename unless STDOUT]
+    perl -nle 'print if /'"$1"'/;' $2
+}
