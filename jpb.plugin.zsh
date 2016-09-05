11794f86903a437a8f786f4947fee8b0f7af2b54
Joe Block <jpb@unixorn.net>
2016-09-04 17:55:46 -0700

    Fix parse error

--- jpb.plugin.zsh
+++ jpb.plugin.zsh
@@ -19,7 +19,11 @@ on_osx()   { [[ "$(uname -s)" = "Darwin" ]] }
 # check if a command is available
 
 function exists() {
-  if (( $+commands[$1] )); then return 0; else return 1;
+  if (( $+commands[$1] )); then
+    return 0
+  else
+    return 1
+  fi
 }
 
 # check if this is an interactive session
