2548422ba2b24dc41952f98ffab14792dfa10275
Joe Block <jpb@unixorn.net>
2021-01-10 07:55:28 -0700

    Add jfrazelle's gostatic function

--- jpb.plugin.zsh
+++ jpb.plugin.zsh
@@ -536,3 +536,47 @@ gogo(){
   # enter the first path found
   cd "${path[0]}"
 }
+
+# build go static binary from root of project
+gostatic(){
+	local dir=$1
+	local arg=$2
+
+	if [[ -z $dir ]]; then
+		dir=$(pwd)
+	fi
+
+	local name
+	name=$(basename "$dir")
+	(
+	cd "$dir" || exit
+  export GOOS=${GOOS:-linux}
+
+	echo "Building ${GOOS} static binary for $name in $dir"
+
+	case $arg in
+		"netgo")
+			set -x
+			go build -a \
+				-tags 'netgo static_build' \
+				-installsuffix netgo \
+				-ldflags "-w" \
+				-o "$name" .
+			;;
+		"cgo")
+			set -x
+			CGO_ENABLED=1 go build -a \
+				-tags 'cgo static_build' \
+				-ldflags "-w -extldflags -static" \
+				-o "$name" .
+			;;
+		*)
+			set -x
+			CGO_ENABLED=0 go build -a \
+				-installsuffix cgo \
+				-ldflags "-w" \
+				-o "$name" .
+			;;
+	esac
+	)
+}
